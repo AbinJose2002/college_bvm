16bb2d7b303f0fb31c15f0d5eae4bf2a5d44b82e
Abin Jose <abinjosofficial307@gmail.com>
2021-11-24 22:08:17 +0530

    lab_c updated with upper triangle

---
+++ lab_c/uppertriangle.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int i, j, row, column, flag = 0;
+    printf("Enter the number of rows and columns");
+    scanf("%d%d",&row,&column);
+    int a[10][10];
+    printf("Enter the elements of matrix");
+    for(i=0;i<row;i++){
+        for(j=0;j<column;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<row;i++){
+        for(j=0;j<i;j++){
+            if(a[i][j]!=0){
+                flag = 1;
+            }
+        }
+    }
+    if(flag == 1){
+        printf("Matrix is not upper triangle");
+    }else{
+        printf("Matrix is upper triangle ");
+    }
+}
