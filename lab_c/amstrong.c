16bc774e100a70e87672da8faa938fc252b6a87a
Abin Jose <abinjosofficial307@gmail.com>
2021-11-24 22:09:00 +0530

    lab_c updated with amstrong number

---
+++ lab_c/amstrong.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int n, num, temp, d, result;
+    printf("Number of digits in number");
+    scanf("%d",&n);
+    printf("enter the digit");
+    scanf("%d",&num);
+    temp = num;
+    while(temp>0){
+        d = temp%10;
+        result+=pow(d,n);
+        n/=10;
+    }
+    if(result==num){
+        printf("Amstrong");
+    }else{
+        printf("not amstrong");
+    }
+    return 0;
+}
