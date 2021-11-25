ff30eba30e14f9299c0dc43ff34a353da100c3a3
Abin Jose <abinjosofficial307@gmail.com>
2021-11-25 18:26:40 +0530

    Amstrog updated in lab c

--- lab_c/amstrong.c
+++ lab_c/amstrong.c
@@ -1,22 +1,42 @@
-// leo sebastian is a killadi
-#include<stdio.h>
-#include<math.h>
-int main(){
-    int n, num, temp, d, result;
-    printf("Number of digits in number");
-    scanf("%d",&n);
-    printf("enter the digit");
+#include <stdio.h>
+#include <math.h>
+int digit;
+int multiply(int);
+int main()
+{
+    int num,temp,rem,result=0;
+    printf("enter the no. of digits in the no.");
+    scanf("%d",&digit);
+    printf("enter the no.");
     scanf("%d",&num);
-    temp = num;
-    while(temp>0){
-        d = temp%10;
-        result+=pow(d,n);
-        n/=10;
+    temp=num;
+    
+    while(temp>0)
+    {
+     rem=temp%10;
+     
+     result+=multiply(rem);
+     
+     temp=temp/10;
+     
     }
-    if(result==num){
-        printf("Amstrong");
-    }else{
-        printf("not amstrong");
-    }
+    
+    if(result==num)
+    printf("armstrong");
+    else
+    printf("not armstrong");
+    
+    
     return 0;
 }
+
+
+  int multiply(rem)
+  {
+      int a=1,i;
+      for(i=1;i<=digit;i++)
+      {
+          a*=rem;
+      }
+      return a;
+ }   
