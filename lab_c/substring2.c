b4eb0473a38eb6fd4a31fd9b1223674600920b5f
Abin Jose <abinjosofficial307@gmail.com>
2021-11-28 19:00:44 +0530

    substring added

--- lab_c/substring2.c
+++ lab_c/substring2.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    char a[50],b[50],i=0,j=0,n=0,m=0,pos,flag=0,k=0,flag2=0;
+    char a[50],b[50],i=0,j=0,n=0,m=0,pos,flag=0,k=0;
     printf("enter a string:");
     gets(a);
     printf("Enter another string:");
@@ -23,23 +23,21 @@ int main()
         if(a[i]==b[0])
         {
             pos=i;
-            for(j=0;j<n;j++)
+            for(j=0;j<n;j++,pos++)
             {
                 if(a[pos]!=b[j])
                 {
                     flag=1;
                 }
-                pos++;
             }
             if(flag==0)
             {
                printf("It is a substring");
-               flag2=1;
                break;
             }
         }
     }
-    if(flag2==0)
+    if(flag==-1)
     {
         printf("It is not a substring");
     }
